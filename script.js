c14805846fb262484b88d11926d33466ce77f470
glenocker <glenocker04@gmail.com>
2025-10-07 22:16:00 -0700

    Correcting inconsistencies

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ document.addEventListener("scroll", () => {
     }
   });
 });
-// Mobile menu toggle
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
 
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+// Mobile menu toggle
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
